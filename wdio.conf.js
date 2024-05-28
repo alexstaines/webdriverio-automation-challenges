6da0a2c5f3e804650a86dbc403531067327bc83c
alexstaines <70612633+alexstaines@users.noreply.github.com>
2024-05-28 11:20:21 +1000

    added chrome headless

--- wdio.conf.js
+++ wdio.conf.js
@@ -67,7 +67,10 @@ export const config = {
     // https://saucelabs.com/platform/platform-configurator
     //
     capabilities: [{
-        browserName: 'chrome'
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+            args: ['headless', 'disable-gpu'],
+        },
     }],
 
     //
